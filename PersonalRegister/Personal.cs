05dd0dc5dab8f4c82c2386afc3097ab53f5ee930
Daniel H <naduran@live.se>
2021-06-15 16:22:55 +0200

    Add project files.

---
+++ PersonalRegister/Personal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    class Personal
+    {
+        public Personal()
+        {
+            register = new List<Tuple<string, double>>();
+        }
+
+        public void AddInfo(string namn, double lön)
+        {
+            register.Add(new Tuple<string,double>(namn, lön));
+        }
+        public void SkrivUt()
+        {
+            int i = 0;
+            foreach (Tuple<string, double> j in register)
+            {
+                
+                Console.WriteLine(register[i]);
+                i++;
+            }
+        }
+
+        public List<Tuple<string,double>> register;
+    }
+}
---
+++ PersonalRegister/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PersonalRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal register = new Personal();
+            register.AddInfo("Johan", 100);
+            register.AddInfo("Dan", 90);
+            register.AddInfo("Mill", 20);
+
+            register.SkrivUt();
+
+
+        }
+    }
+}
